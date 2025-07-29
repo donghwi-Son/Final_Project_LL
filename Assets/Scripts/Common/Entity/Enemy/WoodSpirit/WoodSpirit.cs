b59085d4c84edaf43cc694d43db4e6ebcb4ff91f
Mitaro900 <jhkim9622@gmail.com>
2025-07-29 17:56:09 +0900

    Update WoodSpirit.cs

--- Assets/Scripts/Common/Entity/Enemy/WoodSpirit/WoodSpirit.cs
+++ Assets/Scripts/Common/Entity/Enemy/WoodSpirit/WoodSpirit.cs
@@ -7,4 +7,14 @@ public class WoodSpirit : Enemy
     public WoodSpiritMoveState MoveState { get; private set; }
     public WoodSpiritAttackState AttackState { get; private set; }
     public WoodSpiritDeadState DeadState { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        stateMachine = new StateMachine<WoodSpirit>();
+        IdleState = new WoodSpiritIdleState(this, stateMachine, "Idle");
+        MoveState = new WoodSpiritMoveState(this, stateMachine, "Move");
+        AttackState = new WoodSpiritAttackState(this, stateMachine, "Attack");
+        DeadState = new WoodSpiritDeadState(this, stateMachine, "Dead");
+    }
 }
