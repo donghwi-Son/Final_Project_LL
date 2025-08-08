05eeeb44eacbb6b156a7495a12ab2efd3b20e684
zuhwan0205 <ljh20230379@gmail.com>
2025-08-08 16:58:33 +0900

    BoxAnimation
    BoxAnimation
    - GoldBox, ItemBox 류 이미지 추가
    - 기본 상태 = Close 상자 개봉 시 Close로 변환
    - 상자 오픈 오디오 소스 추가
    - 각 상자 크기, 콜라이더, 스폰 위치 지정

---
+++ Assets/LJH/Script/ItemBox/HardBox.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HardBox : MonoBehaviour, BoxAni
+{
+    [SerializeField] private Sprite openedSprite;
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetOpened()
+    {
+        if (sr != null && openedSprite != null)
+            sr.sprite = openedSprite;
+    }
+}
