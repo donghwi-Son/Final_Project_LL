71d35dec23a6f57e3c7043b0848af706ed136525
zuhwan0205 <ljh20230379@gmail.com>
2025-08-17 15:55:51 +0900

    ItemDebug
    ItemDebug
    - 아이템 효과가 2번 적용되는 버그 수정
    - zone 서브 어택 프리펩이 작동하지 않는 버그 수정

--- Assets/LJH/Script/Sub/SubAttackAoE.cs
+++ Assets/LJH/Script/Sub/SubAttackAoE.cs
@@ -4,19 +4,49 @@ public class SubAttackAoE : SubAttackBase
 {
     [Header("AoE")]
     public float radius = 10f;
+    [SerializeField] private Transform muzzle;
     public int damage = 10;
 
     private readonly Collider2D[] hits = new Collider2D[48];
+    
+    private CharacterStats attacker;
+    
+    public PlayerStats Stats { get; private set; }
+    
+    protected override void OnEquipped()
+    {
+        
+        var pc = owner ? owner.GetComponentInParent<PlayerController>() : null;
+        attacker = pc ? pc.Stats : owner ? owner.GetComponentInParent<CharacterStats>() : null;
+
+        if (!attacker)
+            Debug.LogError($"{name}: 공격자(CharacterStats) 찾기 실패 - owner 경로 확인 필요", this);
+
+        if (!muzzle) muzzle = transform;
+    }
 
     protected override void Fire()
     {
-        int count = Physics2D.OverlapCircleNonAlloc(owner.position, radius, hits, enemyLayer);
-        for (int i = 0; i < count; i++)
+        if (!attacker)
         {
-            var col = hits[i];
-            if (col && col.TryGetComponent(out Enemy enemy)) 
-                //TakeDamage(damage);     //몬스터 데미ㅣ지 
-            hits[i] = null;
+            Debug.LogError($"{name}: attacker == null (OnEquipped에서 못 찾음)", this);
+            return;
+        }
+        if (!muzzle)
+        {
+            Debug.LogError($"{name}: muzzle == null", this);
+            return;
+        }
+        Vector3 pos = muzzle.position;
+        var hits = Physics2D.OverlapCircleAll(pos, radius, enemyLayer);
+        foreach (var col in hits)
+        {
+            if (!col) continue;
+            
+            if (col.TryGetComponent<CharacterStats>(out var enemyStats))
+            {
+                attacker.DoDamage(enemyStats);
+            }
         }
     }
 
